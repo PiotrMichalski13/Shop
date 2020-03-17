ef6475170c6a3577299c7ae559eb0e9e9d8f6e4f
piotrmic13@gmail.com <piotrmic13@gmail.com>
2020-03-17 11:26:09 +0100

    dodanie KursyContext

---
+++ FirstShop/DAL/KursyContext.cs
@@ -0,0 +1,30 @@
+﻿using FirstShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace FirstShop.DAL
+{
+    public class KursyContext :DbContext
+    {
+        public KursyContext(): base("KursyContext")
+        {
+
+        }
+        public DbSet<Kurs> Kursy { get; set; }
+        public DbSet<Kategoria> Kategorie { get; set;}
+        public DbSet<Zamowienie> Zamowienia { get; set; }
+        public DbSet<PozycjaZamowienia> PozycjeZamowienia { get; set; }
+
+        //Usunięcie dodawania końcówki "s" do nazw tabel tworzonych na podstawie tej klasy
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
