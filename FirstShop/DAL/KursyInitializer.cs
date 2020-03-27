ad1b8d8cd0050f2b2f532869c6a31ccb23ed35c6
piotrmic13@gmail.com <piotrmic13@gmail.com>
2020-03-27 07:56:03 +0100

    Autocomplete i filtrowanie ajax

--- FirstShop/DAL/KursyInitializer.cs
+++ FirstShop/DAL/KursyInitializer.cs
@@ -30,13 +30,13 @@ namespace FirstShop.DAL
             context.SaveChanges();
             var kursy = new List<Kurs>
             {
-                new Kurs() {KursId=1, AutorKursu="Piotrek", TytulKursu="Robienie ludzi w balonik vol. 1", KategoriaId=1, CenaKursu=99, Bestseller=true, NazwaPlikuObrazka="obrazekcss.png",
+                new Kurs() {KursId=1, AutorKursu="Piotrek", TytulKursu="ASP.NET MVC", KategoriaId=1, CenaKursu=99, Bestseller=true, NazwaPlikuObrazka="obrazekcss.png",
+                DataDodania=DateTime.Now, OpisKursu="Kurs ASP.NET MVC"},
+                 new Kurs() {KursId=2,AutorKursu="Wiesiek", TytulKursu="CCharp dla początkujących", KategoriaId=1, CenaKursu=99, Bestseller=true, NazwaPlikuObrazka="kursbalonik.png",
                 DataDodania=DateTime.Now, OpisKursu="Jak zrobić w balonik kogoś kto o to prosi"},
-                 new Kurs() {KursId=2,AutorKursu="Wiesiek", TytulKursu="Robienie ludzi w balonik vol. 2", KategoriaId=1, CenaKursu=99, Bestseller=true, NazwaPlikuObrazka="kursbalonik.png",
+                  new Kurs() {KursId=3, AutorKursu="Roman", TytulKursu="Java 11", KategoriaId=3, CenaKursu=99, Bestseller=true, NazwaPlikuObrazka="kursbalonik.png",
                 DataDodania=DateTime.Now, OpisKursu="Jak zrobić w balonik kogoś kto o to prosi"},
-                  new Kurs() {KursId=3, AutorKursu="Roman", TytulKursu="Robienie ludzi w balonik vol. 3", KategoriaId=3, CenaKursu=99, Bestseller=true, NazwaPlikuObrazka="kursbalonik.png",
-                DataDodania=DateTime.Now, OpisKursu="Jak zrobić w balonik kogoś kto o to prosi"},
-                   new Kurs() {KursId=4,AutorKursu="Piotrek", TytulKursu="Robienie ludzi w balonik vol. 4", KategoriaId=1, CenaKursu=99, Bestseller=true, NazwaPlikuObrazka="kursbalonik.png",
+                   new Kurs() {KursId=4,AutorKursu="Piotrek", TytulKursu="C++11", KategoriaId=1, CenaKursu=99, Bestseller=true, NazwaPlikuObrazka="kursbalonik.png",
                 DataDodania=DateTime.Now, OpisKursu="Jak zrobić w balonik kogoś kto o to prosi"}
             };
             kursy.ForEach(k => context.Kursy.AddOrUpdate(k));
